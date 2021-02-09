cd82c772f39eac807ee09d16e5531479082ef06b
Jamtech77 <cucumber6717@gmail.com>
2021-02-09 16:07:00 +0800

    Add Get_Filename function

--- ytdownloader.py
+++ ytdownloader.py
@@ -4,15 +4,26 @@ from os.path import isfile
 import sys
 import re
 
-# st = video.streams.filter(file_extension = "mp4").all()
-# st = video.streams.filter(adaptive=True).all()
-# st = video.streams.filter(only_audio=True).all()
-# st = video.streams.filter(audio_codec="mp4a.40.2").all()
-# st = pytube_object.streams.filter(progressive=True).all()
-# video.streams.get_by_itag(140).download()
+
+def Get_Filename(pytube_obj):
+    # Add Serial Number at the end of filename
+    sequence = 1
+    # Filename = "YouTube%s.mp4"
+    Filename_ori = pytube_obj.title + ".mp4"
+    Filename = pytube_obj.title + "(%s).mp4"
+
+    if isfile(Filename_ori):
+        while isfile(Filename % sequence):
+            sequence = int(sequence or 0) + 1
+        Filename = Filename % sequence
+        Filename = Filename.replace('.mp4','')
+    else:
+        Filename = Filename_ori.replace('.mp4','')
+
+    return Filename
 
 class YouTube_download:
-    def download_by_url(url):
+    def download_by_url(self, url):
         pytube_object = YouTube(url)
         stream = pytube_object.streams.filter(file_extension = "mp4").all()
 
@@ -21,45 +32,18 @@ class YouTube_download:
             print(i)
 
         tag = input("Input tag you want to download:")
+        Filename = Get_Filename(pytube_object)
+        pytube_object.streams.get_by_itag(tag).download(filename = Filename)
 
-        # Add Serial Number at the end of filename
-        sequence = 1
-        # Filename = "YouTube%s.mp4"
-        Filename_ori = pytube_object.title + ".mp4"
-        Filename = pytube_object.title + "(%s).mp4"
-
-        if isfile(Filename_ori):
-            while isfile(Filename % sequence):
-                sequence = int(sequence or 0) + 1
-            Filename = Filename % sequence
-            Filename = Filename.replace('.mp4','')
-            pytube_object.streams.get_by_itag(tag).download(filename = Filename)
-        else:
-            Filename_ori = Filename_ori.replace('.mp4','')
-            pytube_object.streams.get_by_itag(tag).download(filename = Filename_ori)
-
-    def download_music(url):
+    def download_music(self, url):
         pytube_object = YouTube(url)
         # stream = pytube_object.streams.all()
         print(pytube_object.title)  # YouTube video tile
 
-        # Add Serial Number at the end of filename
-        sequence = 1
-        # Filename = "YouTube%s.mp4"
-        Filename_ori = pytube_object.title + ".mp4"
-        Filename = pytube_object.title + "(%s).mp4"
+        Filename = Get_Filename(pytube_object)
+        pytube_object.streams.get_by_itag(140).download(filename = Filename)
 
-        if isfile(Filename_ori):    # Check if existed
-            while isfile(Filename % sequence):
-                sequence = int(sequence or 0) + 1
-            Filename = Filename % sequence
-            Filename = Filename.replace('.mp4','')
-            pytube_object.streams.get_by_itag(140).download(filename = Filename)
-        else:
-            Filename_ori = Filename_ori.replace('.mp4','')
-            pytube_object.streams.get_by_itag(140).download(filename = Filename_ori)
-
-    def download_playlist(url):
+    def download_playlist(self, url):
         playlist = Playlist(url)
         # this fixes the empty playlist.videos list
         playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
@@ -73,38 +57,29 @@ class YouTube_download:
             # for i in stream:
             #     print(i)
             
-            # Add Serial Number at the end of filename
-            sequence = 1
-            # Filename = "YouTube%s.mp4"
-            Filename_ori = pytube_object.title + ".mp4"
-            Filename = pytube_object.title + "(%s).mp4"
-
-            if isfile(Filename_ori):
-                while isfile(Filename % sequence):
-                    sequence = int(sequence or 0) + 1
-                Filename = Filename % sequence
-                Filename = Filename.replace('.mp4','')
-                pytube_object.streams.filter(progressive=True).last().download(filename = Filename)
-            else:
-                Filename_ori = Filename_ori.replace('.mp4','')
-                pytube_object.streams.filter(progressive=True).last().download(filename = Filename_ori)
+            Filename = Get_Filename(pytube_object)
+            pytube_object.streams.filter(progressive=True).last().download(filename = Filename)
 
 
-Select = input("[0] Download video by tag \n[1] Download music \n[2] Download playlist\n")
-if int(Select) not in range(3):
-    print("Invalid input!")
-    exit()
+if __name__ == "__main__":
 
-print("Give URL:")
-url = input()
-try:
-    if int(Select) == 0:
-        YouTube_download.download_by_url(url)
-    elif int(Select) == 1:
-        YouTube_download.download_music(url)
-    elif int(Select) == 2:
-        YouTube_download.download_playlist(url)
-    else:
+    Select_ = input("[0] Download video by tag \n[1] Download music \n[2] Download playlist\n")
+    Select = int(Select_)
+    if Select not in range(3):
         print("Invalid input!")
-except:
-    print("Invalid input!")
+        exit()
+
+    print("Give URL:")
+    url = input()
+    try:
+        yt_dl = YouTube_download()
+        if Select == 0:
+            yt_dl.download_by_url(url)
+        elif Select == 1:
+            yt_dl.download_music(url)
+        elif Select == 2:
+            yt_dl.download_playlist(url)
+        else:
+            print("Invalid input!")
+    except:
+        print("Invalid input!")
